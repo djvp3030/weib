064661cc4b5865b2b7d35b3afc6e626c356d9419
djvp3030 <deividjose.v@gmail.com>
2024-11-30 01:14:27 -0400

    config database

--- weib/settings.py
+++ weib/settings.py
@@ -93,9 +93,9 @@ WSGI_APPLICATION = 'weib.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        "NAME": "dbp",
-        "USER": "postgres",
-        "PASSWORD": "3030",
+        "NAME": "weib",
+        "USER": "weib_user",
+        "PASSWORD": "Ihc8DlXL46TCga6nVcXsJZHOROvAnfyB",
         "HOST": "localhost",
         "PORT": "5432",
     }
@@ -142,7 +142,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 if not DEBUG:
   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
   STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = 'loggin'
 
