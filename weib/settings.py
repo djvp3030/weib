6d5193654829efa6162759f8e5f660f3acb54eeb
djvp3030 <deividjose.v@gmail.com>
2024-12-08 21:22:58 -0400

    trying resolve errors the db

--- weib/settings.py
+++ weib/settings.py
@@ -90,7 +90,14 @@ WSGI_APPLICATION = 'weib.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': os.environ.get('DB_NAME'),
+      'USER': os.environ.get('DB_USER'),  
+      'PASSWORD': os.environ.get('DB_PASSWORD'), 
+      'HOST': os.environ.get('DB_HOST'),  
+      'PORT': '5432',
+    }
 }
 
 
